b216511624cfaee65989f614642e6381077c54e9
Shagan742 <526ssangha@frhsd.com>
2024-11-26 11:50:05 -0500

    front ended the back end

--- script.js
+++ script.js
@@ -1,37 +1,57 @@
 //get username
 let userName=prompt('What\'s your name?');
+
+
+let responseImg=document.getElementById('response-img');
+let responseText=document.getElementById('response-text');
+let greeting=document.getElementById('greeting');
 //input username to website
-userName ? document.getElementById('greeting').innerHTML=(`Hello, ${userName}!`) : document.getElementById('greeting').innerHTML=('Hello stranger!');
+userName 
+? (greeting.innerText=`Hello, ${userName}!`) 
+: (greeting.innerText='Hello stranger!');
+
 
 function shakeMagic8Ball() {
     //get user question
-let userQuestion='Are you okay?';
-console.log(`${userQuestion}`);
+let userQuestion=prompt('What do you wanna ask the 8ball?');
+
 
 //do not touch this
 let randomNumber=Math.floor(Math.random() * 8);
 
-let eightBall=randomNumber;
+let eightBall='';
+let imageChoice='';
 
-if (eightBall===0) {
-  console.log('It is certain');
-} else if(eightBall===1) {
-  console.log('It is decidedly so');
-} else if(eightBall===2) {
-  console.log('Reply hazy try again');
-} else if (eightBall===3){
-  console.log('Cannot predict now');
-} else if(eightBall===4) {
-  console.log('Do not count on it');
-} else if(eightBall===5) {
-  console.log('My sources say no');
-} else if(eightBall===6) {
-  console.log('Outlook not so good');
-} else if (eightBall===7) {
-  console.log('Signs point to yes');
+if (randomNumber==0) {
+  eightBall ='It is certain';
+  imageChoice ='imgs/img1.jpg';
+} else if(randomNumber==1) {
+  eightBall='It is decidedly so';
+  imageChoice='imgs/img2.jpg';
+} else if(randomNumber==2) {
+  eightBall='Reply hazy try again';
+  imageChoice='imgs/img3.jpg';
+} else if(randomNumber==3) {
+  eightBall='Cannot predict now';
+  imageChoice='imgs/img4.jpg';
+} else if(randomNumber==4) {
+  eightBall='Do not count on it';
+  imageChoice='imgs/img5.jpg';
+} else if(randomNumber==5) {
+  eightBall='My sources say no';
+  imageChoice='imgs/img6.jpg';
+} else if(randomNumber==6) {
+  eightBall='Outlook not so good';
+  imageChoice='imgs/img7.jpg';
+} else if(randomNumber==7) {
+  eightBall='Signs point to yes';
+  imageChoice='imgs/img8.jpg';
 } else 
 
 
   console.log(eightBall);
+
+  responseText.innerText=`${userName} asked ${userQuestion} to 8ball. The 8ball replied ${eightBall}`;
+  responseImg.src=imageChoice;
 }
 
